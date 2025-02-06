34172be2041be91cce7d5ceea8496ed01293ed5d
bilal <bnasr.bilal@gmail.com>
2025-02-06 17:24:59 +0000

    commit

--- api/index.js
+++ api/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const serverless = require('serverless-http');
+
 const app = express();
 
-// Exemple d'endpoint GET simple pour tester
+// Route de test
 app.get('/test', (req, res) => {
   res.send("Endpoint test OK");
 });
 
-// Exemple d'endpoint POST pour /chat
+// Si tu veux aussi un endpoint POST pour /chat
 app.post('/chat', (req, res) => {
   res.json({ response: "Réponse test" });
 });
